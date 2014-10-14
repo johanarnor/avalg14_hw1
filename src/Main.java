12797d6744b08760bfa50c3b44eecb0c5b8d02ee
Johan Arnör <johan.arnor@gmail.com>
2014-10-14 20:30:04 +0200

    Modified print function slightly.

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,10 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Random;
 
 public class Main{
-    /*
-    0F9184626272C6A3
-    0323A0F04F9C2409
-    0E70D68977C49E51
-    04F3C9FCADCB63ED
-    0A80341B13BDFEE5
-    0B293A5FB2DE4D6B
-    091D5B5A275D9AA2
-    02C004BAAF622421
-    0997F64331F439F9
-    0CACB15656F6F836
-    064BBDA99B7D5E9E
-    0AA00A1977CB9F29
-    0F0231A69441B70A
-    0851B6608461ACE4
-    0580BFE550CD366B
-    06A4197E1AA5FE5C
-    0725439733DCA8AD
-    038FBE8C4BB0B921
-    08B8B575C94847FB
-    0478DFB56BC69800
-    01BBC39686E97054
-    03A89842DBC06968
-    */
-
     private static final int INPUT_LENGTH = 4194304;
     private static int currentProgress = 0;
 
@@ -75,7 +51,7 @@ public class Main{
                 hashFunctions.put(hash, hashFunction);
             }
         }
-        printToFile(hashFunctions, hashTable);
+        printToFile(h, hashFunctions);
     }
 
     private static boolean isPerfectHash(int[] tempTable) {
@@ -130,18 +106,36 @@ public class Main{
         }
     }
 
-    private static void printToFile(Hashtable<Integer, long[]> hashFunctions, Hashtable<Integer, ArrayList<Long>> hashTable) throws Exception {
+    private static void printToFile(long[] h, Hashtable<Integer, long[]> hashFunctions) throws Exception {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("output.txt"))));
+
+        for (int i = 0; i < h.length; i++) {
+            bw.write(getFullHex(h[i]));
+            bw.newLine();
+        }
+        bw.newLine();
+
+        bw.write("Number of Hi needed = " + hashFunctions.keySet().size());
+        bw.newLine();
+        bw.newLine();
+
         for (int hash : hashFunctions.keySet()) {
             long[] hashFunction = hashFunctions.get(hash);
             bw.write(hash + " " + hashFunction.length);
             bw.newLine();
 
             for (long l : hashFunction) {
-                bw.write(Long.toHexString(l).toUpperCase());
+                bw.write(getFullHex(l));
                 bw.newLine();
             }
         }
         bw.close();
+    }
+
+    private static String getFullHex(long l) {
+        String partHex = Long.toHexString(l).toUpperCase();
+        char[] remainingHex = new char[16 - partHex.length()];
+        Arrays.fill(remainingHex, '0');
+        return new String(remainingHex) + partHex;
     }
 }
