0be2d2bdc4061353294e28eaba5eb996f3e55a41
Johan Arnör <johan.arnor@gmail.com>
2014-10-14 15:57:06 +0200

    Hashing works. A lot of collisions thought.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class Main{
-    private static long[] h;
-
     /*
     0F9184626272C6A3
     0323A0F04F9C2409
@@ -24,22 +25,71 @@ public class Main{
     038FBE8C4BB0B921
     08B8B575C94847FB
     0478DFB56BC69800
-    00BBC39686E97054
+    01BBC39686E97054
     03A89842DBC06968
     */
 
-    public static void main(String[] args) {
-//        long[] h = getH();
-//        System.out.println(Long.parseLong("6725439733DCA8AD"));
-        System.out.println(Long.parseLong("FF9184626272C6A3", 16));
+    private static int INPUT_LENGTH = 4194304;
+
+    public static void main(String[] args) throws Exception {
+        long[] h = getH();
+        long[] hashTable = new long[INPUT_LENGTH];
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(new File("input.txt"))));
+        for (int i = 0; i < INPUT_LENGTH; i++) {
+            long input = Long.parseLong(bf.readLine(), 16);
+            int hash = hash(h, input);
+            hashTable[hash]++;
+        }
+
+        print(hashTable);
     }
 
-    public static long[] getH() {
-        Scanner sc = new Scanner(System.in);
+    private static void print(long[] hashTable) {
+        int sum = 0;
+        int prints = 0;
+        int noZeros = 0;
+        int noOnes = 0;
+        for (int i = 0; i < hashTable.length; i++) {
+            if (hashTable[i] > 2) {
+//                System.out.println(i + "|" + hashTable[i]);
+                prints++;
+            }
+            if (hashTable[i] == 0) {
+                noZeros++;
+            }
+            if (hashTable[i] == 1) {
+                noOnes++;
+            }
+
+            sum += hashTable[i];
+        }
+
+        System.out.println("NO ZEROS = " + noZeros);
+        System.out.println("NO ONES = " + noOnes);
+        System.out.println("NO PRINTS = " + prints);
+        System.out.println("SUM = " + sum);
+    }
+
+    private static int hash(long[] h, long input) {
+        int hash = 0;
+        for (int i = 0; i < h.length; i++) {
+            long longHash = input & h[i];
+            int bitCount = Long.bitCount(longHash);
+            int mod2 = bitCount % 2;
+            hash = hash | mod2;
+            hash = hash << 1;
+        }
+        hash = hash >> 1;
+        return hash;
+    }
+
+    public static long[] getH() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(new File("h.txt"))));
         long[] h = new long[22];
 
         for (int i = 0; i < 22; i++) {
-            h[i] = Long.parseLong(sc.nextLine());
+            h[i] = Long.parseLong(bf.readLine(), 16);
         }
 
         return h;
